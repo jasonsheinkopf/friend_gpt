317c451cfa769c4c0cb25635d8fb5d54109585f7
Jason Sheinkopf <jasonsheinkopf@gmail.com>
2024-09-20 17:30:25 +0900

    updated code port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -12,4 +13,5 @@ def whatsapp_bot():
     return "Message received", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the port from environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)  # Bind to 0.0.0.0 to accept external requests
